58030f4110e75cbfae2265e652c962a17d5fdd80
AyeshaAhmedScripts <ayeshasohailadnanahmed@gmail.com>
2025-07-16 21:42:11 +0500

    Delete Practice 12.py

--- Practice 12.py
+++
@@ -1,9 +0,0 @@
-try:
-    age= int(input("Age: "))
-    income = 2000
-    risk = income/ age
-    print(age)
-except ZeroDivisionError:
-    print("Age can't be zero")
-except ValueError:
-    print("Invalid value")
